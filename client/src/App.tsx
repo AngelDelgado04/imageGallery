cf9ca31f784d520a146cf436bf5cf142711a6c2f
angeleduardodm <angeleduardo200213@gmail.com>
2025-03-19 16:57:34 -0400

    GLI-4 Added status and error messages in case the images do not load correctly.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,19 +8,38 @@ function App() {
   const [images, setImages] = useState<
     Array<{ id: number; src: { medium: string }; alt: string }>
   >([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get(`${import.meta.env.VITE_API_URL}/api/images?query=fascism`)
-      .then((res) => setImages(res.data.photos))
-      .catch((error) => console.error("Error al obtener imágenes", error));
+      .then((res) => {
+        setImages(res.data.photos);
+        setError("");
+      })
+      .catch((error) => {
+        console.error("Error obtaining images.", error);
+        setError("The images could not be loaded. Please try again later.");
+      })
+      .finally(() => setLoading(false));
   }, []);
 
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-1">
-        <ImageGallery images={images}/>
+        <main className="flex-1 pt-16 px-4">
+        {loading ? (
+          <div className="flex justify-center">
+            <p className="text-center text-lg text-secundary pr-4">Loading images</p>
+            <span className="loading loading-spinner text-accent"></span>
+          </div>
+        ) : error ? (
+          <p className="text-center text-red-500">{error}</p>
+        ) : (
+          <ImageGallery images={images} />
+        )}
       </main>
       <Footer />
     </div>
